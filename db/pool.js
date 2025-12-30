6a1c878c16b292e1924d782c4e9611f2098088f4
polymorpheteen <138623315+polymorpheteen@users.noreply.github.com>
2025-12-30 22:05:38 +0100

    Fix the pool connection

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,8 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });

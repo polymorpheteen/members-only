3d59c3aa2e97a4ca3217a3367199013c7fee7149
polymorpheteen <138623315+polymorpheteen@users.noreply.github.com>
2026-01-29 19:10:09 +0100

    Fix app sessions

--- app.js
+++ app.js
@@ -35,9 +35,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24,
     },
   }),
